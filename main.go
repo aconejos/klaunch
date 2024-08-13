9cedb9880103aa168498ca9d977ac8444658dde2
Agustin Conejos <agustin.conejos@mongodb.com>
2024-08-13 11:00:50 +0200

    formatting

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ func main() {
 			mognodbAvailable := check_mongodb_running()
 			if mognodbAvailable != nil {
 				fmt.Println("Error checking for running MongDB:", mognodbAvailable)
-			} 
+			}
 			// Call functions from check_connector_updates.go
 			updateAvailable := check_connector_updates()
 			if updateAvailable != nil {
 				fmt.Println("Error checking for connector updates:", updateAvailable)
-			} 
-			
+			}
+
 			// Spin up docker-compose
-			if updateAvailable == nil  {
+			if updateAvailable == nil {
 				cmd := exec.Command("open", "-a", "Docker")
 				err := cmd.Run()
 				if err != nil {
@@ -41,27 +41,27 @@ func main() {
 				}
 
 				// Start docker-compose
-				cmd = exec.Command("docker-compose","-p", "klaunch", "up", "-d")
+				cmd = exec.Command("docker-compose", "-p", "klaunch", "up", "-d")
 				err = cmd.Run()
 				if err != nil {
 					fmt.Println("Error starting docker-compose:", err)
 				} else {
 					fmt.Println("Klaunch docker-compose started successfully!")
 				}
-			}	
+			}
 		case "stop":
 			// Call stop function
 			fmt.Println("Stopping klaunch...")
 
-			// list all running containers 
-			listContainersCmd := exec.Command("docker-compose","-p", "klaunch", "ps", "-aq")
-			
+			// list all running containers
+			listContainersCmd := exec.Command("docker-compose", "-p", "klaunch", "ps", "-aq")
+
 			output, err := listContainersCmd.Output()
 			if err != nil {
 				fmt.Println("Error listing containers:", err)
 				return
 			}
-			
+
 			containerIDs := strings.Split(string(output), "\n")
 			// remove all the containers
 			for _, id := range containerIDs {
@@ -78,7 +78,7 @@ func main() {
 			fmt.Println("Containers removed successfully!")
 		case "create":
 			fmt.Println("Creating new container...")
-			// Call create function 
+			// Call create function
 			err := create_container()
 			if err != nil {
 				fmt.Println("Error creating new topic:", err)
@@ -112,20 +112,20 @@ func main() {
 			fmt.Println("Extracting logs...")
 			// Get the current date and format it as YYYYMMDD_HHMMSS
 			datePrefix := time.Now().Format("20060102_150405") // Format: YYYYMMDD_HHMMSS
-				
+
 			// Construct the filename with the date prefix
 			filename := fmt.Sprintf("logs/%s_kafka_connect.log", datePrefix)
-				
+
 			// Define the command to get logs from the Docker container
 			cmd := exec.Command("docker", "logs", "kafka-connect")
-				
+
 			// Execute the command and capture the output
 			output, err := cmd.CombinedOutput()
 			if err != nil {
 				fmt.Printf("Failed to execute command: %v\nOutput: %s\n", err, string(output))
 				return
 			}
-		
+
 			// Write the output to a file
 			err = os.WriteFile(filename, output, 0644)
 			if err != nil {
